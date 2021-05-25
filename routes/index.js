a867c4a633e00849f44fdfebab2e492fca4b7bf7
vishapk <53316707+vishapk@users.noreply.github.com>
2021-05-26 02:46:26 +0530

    adding inference

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,31 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+function callPythonScript(req, res) {
+      
+  const { name } = req.body;
+
+  const {spawn} = require("child_process");
+  console.log("\n\n\nBeginning Summarization\n\n\n\n");
+    
+  var python = spawn('python',["./scripts/inference.py",
+      name] );
+
+  python.stdout.on('data', (data) => {
+    console.log("Completed Summarization");
+    var summarizedP=data.toString();
+    console.log(summarizedP);
+    res.json({summary: summarizedP});
+  } );
+    
+  python.stderr.on('data', (data) => {
+    console.log(`error:${data}`);
+  });
+  
+}
+
 // -------- Add This
 // 1. * GET Test. */
-router.post("/api", function(req, res) {
-  res.json({summary : "Lets go"});
-  console.log("Well Done");
-});
+router.post("/api", callPythonScript);
 
 module.exports = router;
